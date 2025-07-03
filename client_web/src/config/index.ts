237deeb77c6e53212918a9358ee8a832d4a2f307
vlkvkn@gmail.com <vlkvkn@gmail.com>
2025-07-03 17:08:12 +0200

    refactor(client): update environment configuration for SUI network and Google API - Modified .env.example to include new SUI network and Google API configurations. - Updated index.ts to utilize environment variables for package and event configuration IDs.

--- client_web/src/config/index.ts
+++ client_web/src/config/index.ts
@@ -1,5 +1,5 @@
-export const PACKAGE_ID = "0x36431e24f17fd9b9e3d8daaa330f0f37b81c3441b157cf7159aa4b8a80c12f1e";
-export const EVENT_CONFIG_ID = "0xd3221c2c75703be7bf1612ff093e711e8b7e630a989984aefdad9b499e5446cd";
+export const PACKAGE_ID = import.meta.env.VITE_SUI_POAPCONTRACT_PACKAGE_ID as string;
+export const EVENT_CONFIG_ID = import.meta.env.VITE_SUI_POAPCONTRACT_EVENT_CONFIG_ID as string;
 export const CLOCK_ID = "0x6";
 
 export const SUI_NETWORK = (import.meta.env.VITE_SUI_NETWORK as "mainnet" | "testnet" | "devnet" | "localnet") ?? "testnet";
